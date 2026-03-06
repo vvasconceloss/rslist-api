5cd66136f94cd6602b63f8d8542ea5531eb5c00b
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-03-06 13:03:48 +0000

    feat(main): handle errors with custom enum (TCP and Axum)

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
+mod errors;
+
 use axum::{Router, routing::get};
 use tracing::{Level, info};
+
+use crate::errors::ServerError;
 
 async fn root() -> &'static str {
     "Hello, World!"
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), ServerError> {
     tracing_subscriber::fmt().with_max_level(Level::INFO).init();
 
     let app = Router::new().route("/", get(root));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
-        .await
-        .expect("Failed");
+    let listener = match tokio::net::TcpListener::bind("0.0.0.0:8080").await {
+        Ok(listener) => listener,
+        Err(_) => return Err(ServerError::TcpListenerError),
+    };
 
     info!("Server running on http://0.0.0.0:8080");
-    axum::serve(listener, app).await.expect("Failed");
+
+    axum::serve(listener, app)
+        .await
+        .map_err(|_e| ServerError::AxumServerError)?;
+
+    Ok(())
 }
