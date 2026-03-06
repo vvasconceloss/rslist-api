56c13efd516bb00a7b6bf6f2f8b9402936a0f225
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-03-06 12:29:52 +0000

    feat(main): start the server on port 8080 with tokio and axum

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,16 @@
-fn main() {}
+use axum::{Router, routing::get};
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
+        .await
+        .expect("Failed");
+
+    axum::serve(listener, app).await.expect("Failed");
+}
